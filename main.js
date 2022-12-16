8a670d0355842e7f895cd8a377f13c707b72c9cd
saankaalppp <sankalp18v@gmail.com>
2022-12-16 19:39:51 +0530

    task12

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ function savetoLocalStorage(e){
     e.preventDefault();
     let name=e.target.Name.value
     let email=e.target.Email.value
-    localStorage.setItem('Name',name)
-    localStorage.setItem('Email',email)
+    // localStorage.setItem('Name',name)
+    // localStorage.setItem('Email',email)
+    let obj={
+        name,email
+    }
+    localStorage.setItem(obj.email,JSON.stringify(obj))
+    showNewUserOnScreen(obj)
+}
+function showNewUserOnScreen(user){
+    const parentNode=document.getElementById('userslist') 
+    const childHtml=`<li> ${user.name} - ${user.email}</li>`
+    parentNode.innerHTML=parentNode.innerHTML+childHtml
 }
